5e0a7b8ae94c595d03222d45478a75a1580a55ca
abdullahjawed01 <abdullahjawed2021@gmail.com>
2025-11-20 15:13:51 +0530

    practice

---
+++ server/app.js
@@ -0,0 +1,42 @@
+// import express from "express"
+// import dotenv from "dotenv"
+// dotenv.config()
+
+// const app = express()
+// const PORT = process.env.PORT
+
+
+// app.get("/",(req,res)=>{
+//     try {
+//         res.status(200).json({msg:"hello this is cfi block code"})
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({msg:error})
+//     }
+// })
+
+// app.listen(PORT,()=>{
+//     console.log(`Helllo the server is running on port http://localhost:${PORT}`);
+// })
+
+
+
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT ;
+
+app.get("/", (req, res) => {
+  try {
+    res.status(200).json({ msg: "hello this is cfi block code" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:PORT`);
+});
